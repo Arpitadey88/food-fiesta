c2711a3ec912bba051c09a13919507a32adb77d3
Arpita Banik <deyarpitabanik@gmail.com>
2024-01-23 23:12:45 +0600

    added product section

--- app/_components/ProductList.jsx
+++ app/_components/ProductList.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-function ProductList() {
+function ProductList({ productList }) {
     return (
-        <div>ProductList</div>
+        <div>
+            {productList.map((item, index) => (
+                <div key={index}>
+                    <h2>{item.attributes.title}</h2>
+                </div>
+            ))}
+        </div>
     )
 }
-
-export default ProductList
+export default ProductList;
--- app/_components/ProductSection.jsx
+++ app/_components/ProductSection.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import ProductList from './ProductList'
+import GlobalApi from '../_utils/GlobalApi'
 
 function ProductSection() {
+    const [productList, setProductList] = useState([]);
+    useEffect(() => {
+        getAllProducts();
+    }, [])
+    const getAllProducts = () => {
+        GlobalApi.getLatestProducts().then(res => {
+            console.log(res.data.data);
+            setProductList(res.data.data)
+        })
+    }
+
     return (
-        <div>
-            <ProductList />
-        </div>
+        (productList &&
+            <div>
+                <ProductList productList={productList} />
+            </div>
+        )
+
     )
 }
 
