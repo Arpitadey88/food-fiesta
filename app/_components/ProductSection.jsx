137760449aaa330095f1e4487d7f048fd00be986
Arpita Banik <deyarpitabanik@gmail.com>
2024-01-27 19:58:05 +0600

    added view all cursor in product section

--- app/_components/ProductSection.jsx
+++ app/_components/ProductSection.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import ProductList from './ProductList'
 import GlobalApi from '../_utils/GlobalApi'
+import { ArrowRightIcon } from 'lucide-react';
 
 function ProductSection() {
     const [productList, setProductList] = useState([]);
@@ -24,13 +25,18 @@ function ProductSection() {
         (productList &&
             <div className='px-10 md:px-20 my-10'>
                 {/* Pizza Items */}
-                <h2 className='font-bold text-[20px] my-3'>Popular Pizaa Items</h2>
+                <h2 className='font-bold text-[20px] my-3'>Popular Pizaa
+                    <span className='font-normal text-[14px] float-right text-blue-600 flex items-center cursor-pointer hover:text-blue-800'>View All Collectoin <ArrowRightIcon className='w-5' /></span></h2>
                 <ProductList productList={filterdProductList('pizza')} />
+
                 {/* Burger Items */}
-                <h2 className='font-bold text-[20px] my-3'>Popular Burger Items</h2>
+                <h2 className='font-bold text-[20px] my-5'>Popular Burger Items
+                    <span className='font-normal text-[14px] float-right text-blue-600 flex items-center cursor-pointer hover:text-blue-800'>View All Collectoin <ArrowRightIcon className='w-5' /></span></h2>
                 <ProductList productList={filterdProductList('burger')} />
+
                 {/* Sandwich Items */}
-                <h2 className='font-bold text-[20px] my-3'>Popular Sandwich Items</h2>
+                <h2 className='font-bold text-[20px] my-5'>Popular Sandwich Items
+                    <span className='font-normal text-[14px] float-right text-blue-600 flex items-center cursor-pointer hover:text-blue-800'>View All Collectoin <ArrowRightIcon className='w-5' /></span></h2>
                 <ProductList productList={filterdProductList('sandwich')} />
             </div>
         )
