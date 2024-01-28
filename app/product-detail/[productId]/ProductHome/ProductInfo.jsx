77144f96226288a8e7fb4169a535439e8feca25c
Arpita Banik <deyarpitabanik@gmail.com>
2024-01-28 23:43:27 +0600

    added add to cart button

--- app/product-detail/[productId]/ProductHome/ProductInfo.jsx
+++ app/product-detail/[productId]/ProductHome/ProductInfo.jsx
@@ -1,3 +1,4 @@
+import { AlertOctagon, BadgeCheck, ShoppingCart } from 'lucide-react'
 import React from 'react'
 
 function ProductInfo({ product }) {
@@ -10,6 +11,10 @@ function ProductInfo({ product }) {
                 <h2 className=' text-[15px] font-medium mt-5 text-gray-800'> {product?.attributes?.whatsIncluded}</h2>
             </div>
             <h2 className='text-[28px] mt-5 text-blue-600 font-medium'>$ {product?.attributes?.pricing}</h2>
+            <h3 className='flex gap-3 mt-5 text-gray-500 font-[13px]'>{product?.attributes?.InstantDelivery ? <BadgeCheck className='text-green-600 h-5 w-5' /> : <AlertOctagon className='text-yellow-300  h-5 w-5' />}Eligible for Instant Delivery</h3>
+            <button className='flex gap-2 p-3 px-10 mt-5 rounded-lg bg-blue-600 text-white hover:bg-blue-800'>
+                <ShoppingCart /> Add to Cart
+            </button>
         </div>
     )
 }
