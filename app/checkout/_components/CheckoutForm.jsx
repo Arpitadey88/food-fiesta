efbc0bbf88005ab82bba670cf0f1a957beb0dd08
Arpita Banik <deyarpitabanik@gmail.com>
2024-02-21 01:28:39 +0600

    added stripe payment method

--- app/checkout/_components/CheckoutForm.jsx
+++ app/checkout/_components/CheckoutForm.jsx
@@ -1,8 +1,15 @@
 import { PaymentElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { useState } from 'react';
 
 const CheckoutForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const [loading, setLoading] = useState(false);
+
+    const handleError = (error) => {
+        setLoading(false);
+        setErrorMessage(error.message)
+    }
 
     const handleSubmit = async (event) => {
         // We don't want to let default form submission happen here,
@@ -14,12 +21,26 @@ const CheckoutForm = () => {
             // Make sure to disable form submission until Stripe.js has loaded.
             return;
         }
-
+        setLoading(true);
+        // Trigger form validation and wallet collection
+        const { error: submitError } = await elements.submit();
+        if (submitError) {
+            handleError(submitError);
+            return;
+        }
+        const res = await fetch("/api/create-intent", {
+            method: "POST",
+            body: JSON.stringify({
+                amount: 5
+            })
+        })
+        const clientSecret = await res.json();
         const result = await stripe.confirmPayment({
             //`Elements` instance that was used to create the Payment Element
+            clientSecret: clientSecret,
             elements,
             confirmParams: {
-                return_url: "https://example.com/order/123/complete",
+                return_url: "http://localhost:3000/",
             },
         });
 
